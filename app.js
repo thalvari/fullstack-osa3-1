c5f9d384d73d6814662420e2aa1d1e93e2709dd5
thalvari <tuomas.halvari@helsinki.fi>
2019-02-11 19:17:25 +0200

    -

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let notes = [
+const express = require('express');
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express();
+
+let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -21,12 +26,48 @@ let notes = [
     },
 ]
 
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
+morgan.token('content', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(bodyParser.json())
+
 app.get('/api/persons', (req, res) => {
-    res.json(notes)
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Puhelinluettelossa ${persons.length} henkilön tiedot</p><p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (body.name === undefined || body.number === undefined) {
+        return res.status(400).json({ error: 'name or number missing' })
+    } else if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({ error: 'name must be unique' })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1e6) + 1,
+    }
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 module.exports = app;
